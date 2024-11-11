4c74dbc26298fb90af4e2bd8816990c04a16bd40
Surpyyy <vincentdesmet10@gmail.com>
2024-11-11 01:39:03 +0100

    add base styles

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,12 @@ export default {
 @import "styles/__variables.scss"; 
 
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: "DM Sans", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     height: 100vh;
+    background-color: #1A1323;
   }
 </style>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,14 +1,20 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/login" v-if="!isLoggedIn">Login</router-link>
-    <router-link to="/register" v-if="!isLoggedIn">Register</router-link>
-    <button @click="logout" v-if="isLoggedIn">Logout</button>
+  <nav class="nav">
+    <div class="title">surpy.</div>
+    <div class="router-item home" :class="isActive('/') ? 'active' : 'inactive'">
+      <router-link to="/" class="nav-item">Home</router-link>
+    </div>
+    <div class="router-item" :class="isActive('/login') ? 'active' : 'inactive'">
+      <router-link to="/login" class="nav-item" v-if="!isLoggedIn" >Login</router-link>
+    </div>
+    <div class="router-item">
+      <button @click="logout" class="nav-item" v-if="isLoggedIn">Logout</button>
+    </div>
   </nav>
   <div>
     <div v-if="user">
       <p>Name: {{ user.name }}</p>
-      <p>Email: {{ user.email }}</p>
+      <p>Email: {{ user.email }}</p> 
       <img :src="user.imageUrl" alt="User Image" v-if="user.imageUrl">
     </div>
   </div>
@@ -16,11 +22,14 @@
 
 <script>
 import { ref, computed, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
 export default {
   name: 'TopNav',
   setup() {
     const user = ref(null);
+    const route = useRoute();
+
 
     onMounted(() => {
       const userName = localStorage.getItem("userName");
@@ -49,11 +58,64 @@ export default {
       window.location.reload(); // Reload the page to reflect changes
     };
 
+    const isActive = (path) => {
+      return route.path === path;
+    };
+
     return {
       user,
       isLoggedIn,
-      logout
+      logout,
+      isActive
     };
   }
 };
 </script>
+
+<style lang="scss">
+@import "@/styles/__variables.scss";
+
+.nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: transparent;
+  color: white;
+  margin: 0 2rem;
+
+  .nav-item {
+    color: white;
+    font-weight: 500;
+    font-size: 1.2rem;
+    margin: 0 1rem;
+    text-decoration: none;
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    &:hover {
+      color: rgb(255, 255, 255, 0.7);
+      transition: color 0.3s;
+    }
+  }
+  
+  .home{
+    margin-left: auto;
+  }
+
+  .title {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  .active {
+    border-bottom: 2px solid $secondary;
+    padding: 0 0 3px;
+  }
+
+  .inactive {
+    padding: 0 0 3px;
+  }
+}
+</style>
